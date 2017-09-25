69d6e869f1a1e467e4395376299ce896c87c647a
robert kraaijeveld <0890289@hr.nl>
2017-09-25 20:22:59 +0200

    Tested basic election parsing

---
+++ Assets/Dataprocessing/CsvParsing/JsonReader.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Dataprocessing.CsvParsing
+{
+    public static class JsonReader<T>
+    {
+        public static T[] ParseJsonToListOfObjects(string fileLocation)
+        {
+            JObject rootObject = JObject.Parse(File.ReadAllText(fileLocation));
+            JArray rootArray = (JArray) rootObject[""];
+
+            T[] objectList = rootArray.ToObject<T[]>();
+            return objectList;
+        }
+    }
+}
