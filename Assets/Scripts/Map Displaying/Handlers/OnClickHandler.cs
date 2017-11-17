b7f13a12cc8608fa703497a84375dab5d0e083f4
RobertKraaijeveld <0890289@hr.nl>
2017-11-17 15:33:44 +0100

    pre-pull

---
+++ Assets/Scripts/Map Displaying/Handlers/OnClickHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Map_Displaying.Handlers
+{
+    public class OnClickHandler : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Mouse pressed");
+                
+                RaycastHit hit;
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Repository/RepositoryHub.cs
@@ -0,0 +1,47 @@
+using Dataformatter.Datamodels;
+using Dataformatter.Dataprocessing.Parsing;
+using Dataformatter.Dataprocessing.Processors;
+using Dataformatter.Data_accessing.Factories.ModelFactories;
+using Dataformatter.Data_accessing.Repositories;
+
+namespace Repository
+{
+    public static class RepositoryHub
+    {
+        public static CountryBordersRepository CountryBordersRepository;
+        public static Iso3166Country[] Iso3166Countries;
+        
+        //todo Again, no call-guarantee
+        public static void Init()
+        {
+            //Parsing
+            SetDataPaths();
+            Iso3166Repository.InitializeCollection();
+            
+            //Countries
+            ParseCountryBorders();
+            CountryBordersRepository = new CountryBordersRepository();
+            Iso3166Countries = Iso3166Repository.GetCollection();
+        }
+
+
+        private static void SetDataPaths()
+        {
+            Dataformatter.Paths.SetProcessedDataFolder(@"E:\Hogeschool\Polishift Organization\ProcessedData\");
+            Dataformatter.Paths.SetRawDataFolder(@"E:\Hogeschool\Polishift Organization\ProcessedData\CountryInformation");
+        }
+
+        private static void ParseCountryBorders()
+        {
+            var geoModelFactory = new CountryGeoModelFactory();
+            var countryGeoJsonPath = Dataformatter.Paths.RawDataFolder;
+
+            var countryBorderModels = JsonToModel<CountryGeoModel>.ParseJsonDirectoryToModels(countryGeoJsonPath,
+                geoModelFactory,
+                "*.geo.json");
+
+            var countryBordersProcessor = new CountryBordersProcessor();
+            countryBordersProcessor.SerializeDataToJson(countryBorderModels);
+        }
+    }
+}
